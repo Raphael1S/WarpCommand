f0ca3024ef46f46219f39dc0d1a96f6bf04e3f87
Raphael S <131126712+Raphael1S@users.noreply.github.com>
2023-07-01 22:48:51 -0300

    Update Teleporte.php

--- WarpCommand/src/Raphael/Teleporte.php
+++ WarpCommand/src/Raphael/Teleporte.php
@@ -34,44 +34,4 @@ class Teleporte extends PluginBase {
             $this->getServer()->getCommandMap()->register($commandName, new WarpCommand($this, $warpName));
         }
     }
-}
-
-class WarpCommand extends Command {
-    private Teleporte $plugin;
-    private string $warpName;
-
-    public function __construct(Teleporte $plugin, string $warpName) {
-        parent::__construct($warpName, "Warp para " . $warpName);
-        $this->plugin = $plugin;
-        $this->warpName = $warpName;
-        $this->setDescription("Warp para {$warpName}");
-        $this->setPermission("warp.comando");
-    }
-
-    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
-        if (!$sender instanceof Player) {
-            $sender->sendMessage(TextFormat::RED . "Este comando só pode ser executado por um jogador!");
-            return false;
-        }
-
-        $config = new Config($this->plugin->getDataFolder() . "warps.yml", Config::YAML);
-        $warpData = $config->get($this->warpName);
-
-        $worldName = $warpData["world"];
-        $x = $warpData["x"];
-        $y = $warpData["y"];
-        $z = $warpData["z"];
-
-        $world = $this->plugin->getServer()->getWorldManager()->getWorldByName($worldName);
-        if ($world === null) {
-            $sender->sendMessage(TextFormat::RED . "O mundo da warp {$this->warpName} não foi encontrado!");
-            return false;
-        }
-
-        $position = new Position($x, $y, $z, $world);
-
-        $sender->teleport($position);
-        $sender->sendMessage(TextFormat::GREEN . "Você foi teleportado!");
-        return true;
-    }
-}
+  }
