ca8e46a7f541e091040c99c04a335c4559acd46c
Raphael S <131126712+Raphael1S@users.noreply.github.com>
2023-07-01 22:49:29 -0300

    Update SetWarpCommand.php

--- WarpCommand/src/Raphael/SetWarpCommand.php
+++ WarpCommand/src/Raphael/SetWarpCommand.php
@@ -8,10 +8,6 @@ use pocketmine\player\Player;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 use pocketmine\world\Position;
-use pocketmine\command\PluginCommand;
-use pocketmine\command\utils\InvalidCommandSyntaxException;
-use pocketmine\permission\Permission;
-use pocketmine\permission\PermissionManager;
 
 class SetWarpCommand extends Command {
 
@@ -23,12 +19,12 @@ class SetWarpCommand extends Command {
         $this->plugin = $plugin;
     }
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args) : bool {
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
         if (empty($args[0])) {
             throw new InvalidCommandSyntaxException();
         }
-        
-$warpName = strtolower($args[0]);
+
+        $warpName = strtolower($args[0]);
 
         $position = $sender->getPosition();
         $world = $position->getWorld()->getFolderName();
@@ -51,53 +47,7 @@ $warpName = strtolower($args[0]);
 
     private function createWarpCommand(string $warpName) {
         $commandName = strtolower($warpName);
-       // $permission = "warp.comando";
 
-     //   $permissionManager = PermissionManager::getInstance();
-     //   $permission = new Permission($permission);
-     //   $permissionManager->addPermission($permission);
-    //    $this->setPermission($permission->getName());
-
-        $this->plugin->getServer()->getCommandMap()->register($commandName, new class($this->plugin, $warpName) extends Command {
-            private Teleporte $plugin;
-            private string $warpName;
-
-            public function __construct(Teleporte $plugin, string $warpName) {
-                parent::__construct($warpName, "Warp para " . $warpName);
-                $this->plugin = $plugin;
-                $this->warpName = $warpName;
-                $this->setDescription("Warp para {$warpName}");
-     $this->setPermission("warp.comando");
-            }
-            
-public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
-if (!$sender instanceof Player) {
-            $sender->sendMessage(TextFormat::RED . "Este comando só pode ser executado por um jogador!");
-            return false;
-        }
-
-    $config = new Config($this->plugin->getDataFolder() . "warps.yml", Config::YAML);
-    $warpData = $config->get($this->warpName);
-
-
-    $worldName = $warpData["world"];
-    $x = $warpData["x"];
-    $y = $warpData["y"];
-    $z = $warpData["z"];
-
-    $world = $this->plugin->getServer()->getWorldManager()->getWorldByName($worldName);
-    if ($world === null) {
-        $sender->sendMessage(TextFormat::RED . "O mundo da warp {$this->warpName} não foi encontrado!");
-        return false;
-    }
-
-    $position = new Position($x, $y, $z, $world);
-
-    $sender->teleport($position);
-    $sender->sendMessage(TextFormat::GREEN . "Você foi teleportado!");
-    return true;
-}
-
-        });
+        $this->plugin->getServer()->getCommandMap()->register($commandName, new WarpCommand($this->plugin, $warpName));
     }
 }
