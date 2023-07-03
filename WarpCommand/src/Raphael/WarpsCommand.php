c48a4d5b6efe251041b29e294aa386c838303289
Raphael S <131126712+Raphael1S@users.noreply.github.com>
2023-07-03 12:51:06 -0300

    Create WarpsCommand.php

---
+++ WarpCommand/src/Raphael/WarpsCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Raphael;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+
+class WarpsCommand extends Command {
+
+    private Teleporte $plugin;
+
+    public function __construct(Teleporte $plugin) {
+        parent::__construct("warps", "Mostra as warps disponíveis");
+        $this->setPermission("warps.warps");
+        $this->plugin = $plugin;
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        $config = new Config($this->plugin->getDataFolder() . "warps.yml", Config::YAML);
+        $warps = $config->getAll();
+
+        if (empty($warps)) {
+            $sender->sendMessage(TextFormat::YELLOW . "Não existem warps no servidor.");
+        } else {
+            $sender->sendMessage(TextFormat::YELLOW . "Warps disponíveis:");
+            foreach ($warps as $warpName => $warpData) {
+                $sender->sendMessage(TextFormat::GREEN . "- " . $warpName);
+            }
+        }
+
+        return true;
+    }
+}
