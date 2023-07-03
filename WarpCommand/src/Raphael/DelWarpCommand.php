6f1b221f19b22e34dd0aca985ec0adae8983bbca
Raphael S <131126712+Raphael1S@users.noreply.github.com>
2023-07-03 08:53:48 -0300

    Creating delwarp command class

---
+++ WarpCommand/src/Raphael/DelWarpCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Raphael;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+
+class DelWarpCommand extends Command {
+
+    private Teleporte $plugin;
+
+    public function __construct(Teleporte $plugin) {
+        parent::__construct("delwarp", "Deleta uma warp existente");
+        $this->setPermission("delwarp.delwarp");
+        $this->plugin = $plugin;
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        if (empty($args[0])) {
+            throw new InvalidCommandSyntaxException();
+        }
+
+        $warpName = strtolower($args[0]);
+
+        $config = new Config($this->plugin->getDataFolder() . "warps.yml", Config::YAML);
+
+        if ($config->exists($warpName)) {
+            $config->remove($warpName);
+            $config->save();
+            $sender->sendMessage(TextFormat::GREEN . "A warp {$warpName} foi deletada!");
+        } else {
+            $sender->sendMessage(TextFormat::RED . "A warp {$warpName} não existe!");
+        }
+
+        // Remover comando de warp
+        $this->removeWarpCommand($warpName);
+
+        return true;
+    }
+
+    private function removeWarpCommand(string $warpName) {
+        $commandName = strtolower($warpName);
+
+        $commandMap = $this->plugin->getServer()->getCommandMap();
+        $command = $commandMap->getCommand($commandName);
+
+        if ($command !== null && $command instanceof WarpCommand) {
+            $commandMap->unregister($command);
+        }
+    }
+}
