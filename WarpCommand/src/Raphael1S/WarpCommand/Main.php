6e940b69e82345b3766785616bf14e797acc05ee
Raphael S <131126712+Raphael1S@users.noreply.github.com>
2023-07-06 01:34:34 -0300

    Create Main.php

---
+++ WarpCommand/src/Raphael1S/WarpCommand/Main.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Raphael1S\WarpCommand;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase {
+
+    /** @var array */
+    private $warps;
+
+    public function onEnable(): void {
+        // Load saved warps
+        
+        $this->warps = (new Config($this->getDataFolder() . "warps.yml", Config::YAML))->getAll();
+
+// Register the /delwarp command
+$this->getServer()->getCommandMap()->register("WarpCommand", new DelWarpCommand($this));
+
+// Register the /setwarp command
+$this->getServer()->getCommandMap()->register("WarpCommand", new SetWarpCommand($this));
+
+// Register the /warps command
+$this->getServer()->getCommandMap()->register("WarpCommand", new WarpsCommand($this));
+        
+        // Register existing warp commands
+        $this->registerWarpCommands();
+    }
+
+    public function registerWarpCommands(): void {
+        foreach ($this->warps as $warpName => $warpData) {
+            $commandName = strtolower($warpName);
+            
+ $this->getServer()->getCommandMap()->register("WarpCommand", new WarpCommand($this, $warpName));
+       }
+    }
+}
